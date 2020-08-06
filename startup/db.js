a4c253e989d95833b3d1a678d7fe993e948f8b71
KellyEgesa <kelly.egesa@gmail.com>
2020-08-06 17:05:18 +0300

    Corrected an error

--- startup/db.js
+++ startup/db.js
@@ -5,9 +5,7 @@ module.exports = function () {
   const db = config.get("db");
   mongoose
     .connect(
-      `mongodb://KellyEgesa:${config.get(
-        "passworddb"
-      )}@ds045465.mlab.com:45465/heroku_d4vwwmnb`,
+      `mongodb://KellyEgesa:Bartholomew@1007@cluster0.bkfbu.mongodb.net/<dbname>?retryWrites=true&w=majority`,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
